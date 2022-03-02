ac51776a84206273611b01084d214e33286446e4
johannesnicolaus <johannes.nicolaus@gmail.com>
2022-03-02 15:03:38 +0900

    fix shiny codes

--- inst/visualization/app/server.R
+++ inst/visualization/app/server.R
@@ -1,14 +1,4 @@
-library(shiny)
-library(tidyverse)
-
-
-
 # plot
-
-
-
-
-
 shinyServer(function(input, output) {
   # show the data frame
   output$contents <- renderTable({
@@ -18,72 +8,63 @@ shinyServer(function(input, output) {
     # column will contain the local filenames where the data can
     # be found.
     inFile <- input$file1
-    
+
     if (is.null(inFile))
       return(NULL)
-    
-    read_csv(inFile$datapath)
+
+    read.csv(inFile$datapath, stringsAsFactors = F)
   }, align = "c", width = "100%")
-  
+
   # print table dimensions
-  output$tbl_dims <- renderText({ 
+  output$tbl_dims <- renderText({
     inFile <- input$file1
-    
+
     if (is.null(inFile))
       return(NULL)
-    
-    csvfile <- read_csv(inFile$datapath)
-    
+
+    csvfile <- read.csv(inFile$datapath)
+
     paste("Number of samples:" , dim(csvfile)[1])
   })
-  
+
 
 # renderplot --------------------------------------------------------------
 
   output$heatscatterplot <- renderPlot({
-    
-    heatscatter <- function(data, color_scale = scale_color_viridis_c(), 
-                            line_color = "#ef3b2c", 
-                            line_fun = "median", 
-                            jitterwidth = 0.1, 
-                            x_label = "Time (min)", 
-                            y_label = "log2(Foci number + 1)", 
+
+    heatscatter <- function(data, color_scale = ggplot2::scale_color_viridis_c(),
+                            line_color = "#ef3b2c",
+                            line_fun = "median",
+                            jitterwidth = 0.1,
+                            x_label = "Time (min)",
+                            y_label = "log2(Foci number + 1)",
                             title = element_blank()){
-      require(tidyverse)
-      require(reshape2)
 
       data1 <- data
-      
-      factor_time <- data1 %>% colnames()
-      
-      # transformed data
-      
-      
-      if (input$logfunc_data == "log2_1") {
-        log_transformed <- log2(data1+1)
-      } else if (input$logfunc_data == "loge_1") {
-        log_transformed <- log(data1+1)
-      } else if (input$logfunc_data == "log10_1") {
-        log_transformed <- log10(data1+1)
-      } else {log_transformed <- log2(data1+1)}
-      
 
-      # perform log2(n+1) on the foci to not remove cells with 0 foci
-      data_melt <- melt(log_transformed) %>%
-        na.omit() %>% 
-        mutate(variable_1 = variable)
-      
+      factor_time <- data1$time
+
+      # transformed data
+      if (input$logfunc_data == "log2_1") {
+        log_transformed <- dplyr::mutate(data1, foci_count = log2(foci_count +1))
+      } else if (input$logfunc_data == "loge_1") {
+        log_transformed <- dplyr::mutate(data1, foci_count = log(foci_count +1))
+      } else if (input$logfunc_data == "log10_1") {
+        log_transformed <- dplyr::mutate(data1, foci_count = log10(foci_count +1))
+      } else {log_transformed <- dplyr::mutate(data1, foci_count = log2(foci_count +1))}
+
+
       res <- c()
-      # get density
-      
-      for(i in factor_time){
-        variable_filter <- filter(data_melt, variable == i)
-        variable_filter <- arrange(variable_filter, value)
-        variable_filter$Density<- density(variable_filter$value, n=length(variable_filter$value))$y
-        res <- bind_rows(res, variable_filter)  
+
+      # calculate density at each time point
+      for(i in unique(factor_time)){
+        variable_filter <- dplyr::filter(log_transformed, time == i)
+        variable_filter <- dplyr::arrange(variable_filter, foci_count)
+        variable_filter$Density<- stats::density(variable_filter$foci_count, n=length(variable_filter$foci_count))$y
+        res <- bind_rows(res, variable_filter)
       }
-      
-      
+
+
       # theme
       theme_Publication <- function(base_size=40) {
        theme_bw(base_size=base_size)+
@@ -95,7 +76,7 @@ shinyServer(function(input, output) {
                   axis.title = element_text(size = rel(0.8)),
                   #            axis.title.y = element_text(angle=90,vjust =2),
                   #            axis.title.x = element_text(vjust = -0.2),
-                  axis.text = element_text(), 
+                  axis.text = element_text(),
                   #            axis.line = element_line(colour="black"),
                   axis.ticks = element_line(),
                   panel.grid.major = element_blank(),
@@ -109,39 +90,39 @@ shinyServer(function(input, output) {
                   strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
                   strip.text = element_text(face="bold")
           )
-        
+
       }
-      
-      
-      # plot    
-      p <- ggplot(res, aes(x=variable, y=value, color = Density))
+
+
+      # plot
+      p <- ggplot(res, aes(x=as.factor(time), y=foci_count, color = Density))
       p2 <- p +
-        geom_jitter(shape=16, width = jitterwidth, height = 0,size= 4, alpha =1)+ 
+        geom_jitter(shape=16, width = jitterwidth, height = 0,size= 4, alpha =1)+
         labs(x = x_label, y = y_label, color ="Density\n(a.u.)") +
         # labs(x = element_blank(), y = element_blank())+
-        stat_summary(aes(group=1), fun=line_fun, geom="line", size=2, color= line_color)+ 
-        stat_summary(aes(group=1), fun=line_fun, geom="point", size=6, color= line_color)+ 
-        color_scale + 
+        stat_summary(aes(group=1), fun=line_fun, geom="line", size=2, color= line_color)+
+        stat_summary(aes(group=1), fun=line_fun, geom="point", size=6, color= line_color)+
+        color_scale +
         theme_Publication() +
         ylim(input$sidebar_yaxis_range_data[1],input$sidebar_yaxis_range_data[2])+
-        ggtitle(title) 
-      
-      
-      print(p2)  
+        ggtitle(title)
+
+
+      print(p2)
     }
-    
+
     # read file
     inFile <- input$file1
-    
+
     if (is.null(inFile))
       return(NULL)
-    
-    heatscatter(read_csv(inFile$datapath), 
+
+    heatscatter(read.csv(inFile$datapath),
                 line_fun = input$sumfunc_data,
                 y_label = input$sidebar_yaxistitle_data,
                 x_label = input$sidebarxaxistitle_data,
                 title = input$plot_title_data,
                 jitterwidth = input$jitterwidth_data)
-    
+
   })
 })
--- inst/visualization/app/ui.R
+++ inst/visualization/app/ui.R
@@ -1,7 +1,3 @@
-library(shiny)
-
-
-
 # fileinput
 file_input <- fileInput("file1", "Choose csv File",
                         accept = c(
@@ -11,12 +7,11 @@ file_input <- fileInput("file1", "Choose csv File",
 )
 
 # summary function
-
-sumfunc <- radioButtons("sumfunc_data", h4("Summarization method"), 
+sumfunc <- radioButtons("sumfunc_data", h4("Summarization method"),
             choices = list("median" = "median", "mean" = "mean"), selected = "median")
 
 # logfunc
-logfunc <- radioButtons("logfunc_data", h4("Data transformation method"), 
+logfunc <- radioButtons("logfunc_data", h4("Data transformation method"),
                         choices = list("log2(n+1)" = "log2_1", "ln(n+1)" = "loge_1",
                                        "log10(n+1)" = "log10_1"), selected = "log2_1")
 
@@ -26,49 +21,38 @@ logfunc <- radioButtons("logfunc_data", h4("Data transformation method"),
 jitterwidth <- sliderInput("jitterwidth_data", "Jitter width",
                            min = 0, max = 1, value = c(0.1))
 
-# color palette
-
-# colpalette <- radioButtons("colpalette_data", h4("Select which color palette to use"), 
-#                         choices = list("viridis" = "scale_color_viridis()", "blue_black" = "scale_color_continuous()"), selected = 1)
-
-
 # x axis
-sidebar_xaxistitle <- textInput("sidebarxaxistitle_data", h4("X axis title"), 
-          value = "Time (min)")  
-
-# sidebar_xaxis_range <- sliderInput("sidebar_xaxis_range_data", "X axis range",
-#                                    min = 0, max = 100, value = c(25, 75))
+sidebar_xaxistitle <- textInput("sidebarxaxistitle_data", h4("X axis title"),
+          value = "Time (min)")
 
 # y axis
-sidebar_yaxistitle <- textInput("sidebar_yaxistitle_data", h4("Y axis title"), 
-                           value = "log2(Foci number + 1)")  
+sidebar_yaxistitle <- textInput("sidebar_yaxistitle_data", h4("Y axis title"),
+                           value = "log2(Foci number + 1)")
 
 sidebar_yaxis_range <- sliderInput("sidebar_yaxis_range_data", "Y axis range",
                                    min = 0, max = 20, value = c(0, 6), step = 0.5)
 
-# plot title 
-plot_title <- textInput("plot_title_data", h4("Plot title"), 
-                        value = "Enter text...")  
+# plot title
+plot_title <- textInput("plot_title_data", h4("Plot title"),
+                        value = "Enter text...")
 
-# Define UI 
+# Define UI
 shinyUI(pageWithSidebar(
-  
+
   # Application title
   headerPanel("Heatscatter plot of foci"),
-  
+
   sidebarPanel(
     file_input,
     sumfunc,
     jitterwidth,
     logfunc,
-    # colpalette,
     plot_title,
     sidebar_xaxistitle,
-#    sidebar_xaxis_range,
     sidebar_yaxistitle,
     sidebar_yaxis_range
   ),
-  
+
   mainPanel(plotOutput("heatscatterplot", height = "600px"),
             textOutput("tbl_dims"),
             div(style="height:600px; overflow:scroll; align:text-center",
